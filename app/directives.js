8b92ac5755c5110c6ca752b8a30a2033e0b37d85
FoxxMD <matt.duncan13@gmail.com>
2014-05-02 11:27:01 -0400

    added responsive interaction for container at higher resolutions on photos page switched masonry layout to percentages and improved styling implemented poptrox lightbox

--- app/directives.js
+++ app/directives.js
@@ -5,19 +5,21 @@ angular.module('app.directives', ['wu.masonry'])
             templateUrl: '/app/templates/photos.html',
             controller:function($scope){
                 $scope.bricks = [
-                    {src: "http://lorempixel.com/250/300/"},
-                    {type:'w2', src: "http://lorempixel.com/500/350/"},
-                    {type:'', src: "http://lorempixel.com/250/300/"},
-                    {src: "http://lorempixel.com/250/150/"},
-                    {src: "http://lorempixel.com/250/350/"},
-                    {type:'w2', src: "http://lorempixel.com/500/250/"},
-                    {src: "http://lorempixel.com/250/300/"},
-                    {src: "http://lorempixel.com/250/200/"},
-                    {type:'w2', src: "http://lorempixel.com/500/300/"},
-                    {type:'', src: "http://lorempixel.com/250/150/"}
+                    {src: "http://th01.deviantart.net/fs70/PRE/f/2012/362/9/4/watch_me_by_matthewfoxxphotos-d5pge3s.jpg"},
+                    {type:'w2', src: "http://lorempixel.com/800/500/"},
+                    {type:'', src: "http://lorempixel.com/600/800/"},
+                    {src: "http://lorempixel.com/500/700/"},
+                    {type:'w2', src: "http://lorempixel.com/800/550/"},
+                    {type:'w2', src: "http://lorempixel.com/900/600/"},
+                    {src: "http://lorempixel.com/400/600/"},
+                    {src: "http://lorempixel.com/500/700/"},
+                    {type:'w2', src: "http://lorempixel.com/700/500/"},
+                    {type:'', src: "http://lorempixel.com/500/750/"}
                 ];
             },
             link: function (scope, element, attrs) {
+
+
 
                 var photos = $('.showcasePane'),
                     showcaseHeight = photos.innerHeight();
@@ -26,9 +28,9 @@ angular.module('app.directives', ['wu.masonry'])
                     $(element).find('img').removeClass('showPhoto');
                     photos.removeAttr('style');
                 });
+                var imagesLoaded = false;
                 scope.$on('masonry.layoutComplete', function(event, message){
                     message = message + 100;
-                    var imagesLoaded = false;
                     if(showImages){
                         $timeout.cancel(showImages);
                     }
@@ -43,6 +45,11 @@ angular.module('app.directives', ['wu.masonry'])
                                 $timeout(function () {
                                     $(item).addClass('showPhoto');
                                 }, waittime);
+                            });
+                            var gallery = $(element).find('.masonryContainer');
+                            gallery.poptrox({
+                                usePopupNav: true,
+                                popupPadding: 0
                             });
                         }, 0);
                     }
