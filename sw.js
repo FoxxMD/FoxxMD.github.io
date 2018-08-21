f0b80d764760108b561c81e8925ed95d47dde9a1
FoxxMD <matt.duncan13@gmail.com>
2018-08-21 15:30:33 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-5534995e6fe9d5481453.js","86ec10e4f837325eb11326e9349452d3"],["/app-72fee159017865b2b81e.js","b8357aa4e6fbb6fa8a74626ca6011bc2"],["/app-c51a139eb0923c5b9c85.js","6d1bc41042900685f5bf3c41a6b9650a"],["/commons-560c04d0d3cbafa16c34.js","4b5f5c9b0ace02c35b04c531c928f061"],["/index.html","4dee7985d3752d56390643e433ff6a0d"],["/manifest.json","9a47c29d5a3d9342bfc463794d412643"],["/offline-plugin-app-shell-fallback/index.html","a3ccd9970ec742648f913461fea9b99d"]];
+var precacheConfig = [["/app-5534995e6fe9d5481453.js","86ec10e4f837325eb11326e9349452d3"],["/app-72fee159017865b2b81e.js","b8357aa4e6fbb6fa8a74626ca6011bc2"],["/app-c51a139eb0923c5b9c85.js","6d1bc41042900685f5bf3c41a6b9650a"],["/app-eb9310270165168e6b9f.js","d7f964ca68d35b758fa2951378eac2dd"],["/commons-560c04d0d3cbafa16c34.js","4b5f5c9b0ace02c35b04c531c928f061"],["/index.html","65f515b7b705bd1c1e40fb2bbdac872f"],["/manifest.json","9a47c29d5a3d9342bfc463794d412643"],["/offline-plugin-app-shell-fallback/index.html","665bdd5a68d1ad90e3ea0843b4c63c13"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
